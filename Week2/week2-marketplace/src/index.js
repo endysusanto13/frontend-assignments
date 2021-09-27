793595e1b08db3605bb4fca59854d2cba0417a51
endysusanto13 <endy.susanto@gmail.com>
2021-09-27 12:38:07 +0800

    Initialize Week 2 Marketplace assignment

---
+++ Week2/week2-marketplace/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Marketplace } from "./pages/marketplace";
+
+ReactDOM.render(<Marketplace />, document.querySelector("#root"));
