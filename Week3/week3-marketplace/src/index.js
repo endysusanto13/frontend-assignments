26d22fc444bec098936e118d045fb2084cf28f36
endysusanto13 <endy.susanto@gmail.com>
2021-09-29 20:46:00 +0800

    Initialize Week 3 assignment from doIt-31 branch
    Only required files are copied.

---
+++ Week3/week3-marketplace/src/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { AuthProvider } from "./domains/auth";
+import "./index.css";
+import { MarketplacePublic } from "./pages/marketplace-public";
+
+ReactDOM.render(
+  <AuthProvider>
+    <MarketplacePublic />
+  </AuthProvider>
+  ,document.querySelector("#root")
+);
